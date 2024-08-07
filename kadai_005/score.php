b68fc8c871659bbd4226a6d61565d44a597b8014
arata-Engineer <arataspace1@gmail.com>
2024-08-07 19:16:07 +0900

    php01_kadai_005

---
+++ kadai_005/score.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>score課題</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // 四則演算(平均値)
+        $scores = 80+60+55+40+100+25+80+95+30+60;
+        echo $scores/10;
+         ?>
+     </p>
+ </body>
+ 
+ </html>
