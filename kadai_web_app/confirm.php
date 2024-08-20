216695095ee0d15db534d9e1a6072fe82c9f6de4
arata-Engineer <arataspace1@gmail.com>
2024-08-20 19:15:01 +0900

    commit_kadai_web_app

---
+++ kadai_web_app/confirm.php
@@ -0,0 +1,55 @@
+<?php
+// POSTリクエストから入力データを取得
+$name = $_POST['user_name'];
+$age = $_POST['user_age'];
+$department = $_POST['department'];
+
+$errors = [];
+
+if(empty($name)){
+    $errors[] = '社員名を入力して下さい';
+}
+if(empty($age)){
+    $errors[] = '年齢を入力して下さい'; 
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_web_app</title>
+</head>
+
+<body>
+    <h2>入力内容をご確認ください。</h2>
+    <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+
+    <table border="1">
+        <tr>
+            <th>項目</th>
+            <th>入力内容</th>
+        </tr>
+        <tr>
+            <td>社員名</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>年齢</td>
+            <td><?php echo $age; ?></td>
+        </tr>
+        <tr>
+            <td>所属部署</td>
+            <td><?php echo $department; ?></td>
+        </tr>
+    </table>
+
+    <p>
+        <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+        <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+    </p>
+</body>
+
+</html>
