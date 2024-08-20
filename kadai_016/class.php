55e7786f106f065273f224dae7378a27c245c312
arata-Engineer <arataspace1@gmail.com>
2024-08-20 15:24:12 +0900

    commit02_kadai_016

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -2,8 +2,10 @@
 class Food {
     private $name;
     private $price;
+
     public function show_price(int $price) {
-        $this->price = $price;
+        $this->price = 250;
+        echo $this->price . '<br>';
     }
     public function __construct(string $name, int $price) {
         $this->name = $name;
@@ -19,7 +21,8 @@ class Animal {
     private $height;
     private $weight;
     public function show_height(int $height) {
-        $this->height = $height;
+        $this->height = 60;
+        echo $this->height . '<br>';
     }
     public function __construct(string $name, int $height, int $weight) {
             $this->name = $name; 
@@ -36,3 +39,5 @@ $food->show_price(250);
 $animal->show_height(60);
 
 ?>
+
+
